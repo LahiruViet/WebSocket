8a662c817007385690641d7bd9f4b0d0769f9d19
LahiruViet <48759351+LahiruViet@users.noreply.github.com>
2019-05-01 13:52:04 +0530

    add initial source

---
+++ src/main/java/itsj/research/websocket/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package itsj.research.websocket.config;
+
+import itsj.research.websocket.handler.SocketTextHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@EnableWebSocket
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketTextHandler(), "/user");
+    }
+}
